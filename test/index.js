e1a0141caf460694e1e677043f8d4b933cd6c0e1
Michael Cavalea <callmecavs@gmail.com>
2017-01-22 18:07:21 -0500

    scaffold api in test file

--- test/index.js
+++ test/index.js
@@ -6,3 +6,7 @@ const assert = require('chai').assert
 const path = require('path')
 
 const handmade = require('../index.js')
+
+handmade(__dirname)
+  .read('./folder')
+  .then(() => console.log('done'))
