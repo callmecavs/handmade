879c0e320c01413e7e89bd067a7d74f27d26e7b6
Michael Cavalea <callmecavs@gmail.com>
2017-01-22 21:25:42 -0500

    linting

--- examples/empty-transform.js
+++ examples/empty-transform.js
@@ -3,7 +3,6 @@
 // a valid transform is simply a function that returns a Promise
 // a contents parameter is passed in (path -> content pairs)
 const transform = contents => new Promise((resolve, reject) => {
-
   // remember to resolve the transformed contents object
   // remember to include the contents you didnt modify
   resolve(contents)
