6f3a7f4c7cc6fed35c2bd76732ff4d2e5d861607
Michael Cavalea <callmecavs@gmail.com>
2017-01-22 21:24:03 -0500

    add empty transform

---
+++ examples/empty-transform.js
@@ -0,0 +1,12 @@
+'use strict'
+
+// a valid transform is simply a function that returns a Promise
+// a contents parameter is passed in (path -> content pairs)
+const transform = contents => new Promise((resolve, reject) => {
+
+  // remember to resolve the transformed contents object
+  // remember to include the contents you didnt modify
+  resolve(contents)
+})
+
+module.exports = transform
