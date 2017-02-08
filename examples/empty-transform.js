4623457d093deba3c675651813b1e37bf5a10064
callmecavs <callmecavs@gmail.com>
2017-02-07 19:36:42 -0500

    update empty transform example

--- examples/empty-transform.js
+++ examples/empty-transform.js
@@ -1,8 +1,11 @@
 'use strict'
 
-// a valid transform is simply a function that returns a Promise
-// a contents parameter is passed in (path -> content pairs)
+// must accept the build contents object
+// must return a Promise
 const transform = contents => new Promise((resolve, reject) => {
+  // the fileset is stored in contents.files, an object of paths -> contents
+  console.log(contents.files)
+
   // remember to resolve the transformed contents object
   // remember to include the contents you didnt modify
   resolve(contents)
